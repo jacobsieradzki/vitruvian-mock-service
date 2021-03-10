ca01630031555eec1a0ea38c549cbf03f0ffc64a
Jake Ryan <jake@springnation.co.uk>
2021-03-10 20:22:09 +0000

    Added time query

--- pages/api/mock/mpu.js
+++ pages/api/mock/mpu.js
@@ -4,7 +4,10 @@ export default function getRequestHandler(req, res) {
   const { query } = req;
 
   const date = new Date();
-  const timestamp = date.getTime();
+  var seed = date.getTime();
+
+  var time = parseFloat(query?.time)
+  if (time) seed = time;
 
   var x = 0;
   var y = 0;
@@ -13,16 +16,16 @@ export default function getRequestHandler(req, res) {
   switch (query.id || '') {
     case 'test1_mpu1':
       x = 0;
-      y = 2.5 * Math.sin(timestamp + 2.5);
+      y = 2.5 * Math.sin(seed + 2.5);
       z = 1;
     case 'test1_mpu2':
       x = 0;
-      y = 3 * Math.sin(1.5 * timestamp);
+      y = 3 * Math.sin(1.5 * seed);
       z = 1;
     default:
       break;
   }
 
-  const output = `${timestamp},${x},${y},${z}`;
+  const output = `${seed},${x},${y},${z}`;
   getRequestTextResponse(req, res, output);
 }
